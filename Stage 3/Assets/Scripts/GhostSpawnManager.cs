eee3fa831bafc5c296c8d2da3ec05bf0f68109cb
BoyanKardzhiev <b.r.kardzhiev@student.tue.nl>
2023-11-21 11:58:20 +0100

    Initial Code Upload

---
+++ Stage 3/Assets/Scripts/GhostSpawnManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class GhostSpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    ARRaycastManager m_RaycastManager;
+    List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
+
+    [SerializeField] 
+    GameObject spawnablePrefab;
+    public List<GameObject> spawnedObjects = new List<GameObject>();
+    Vector3 spawnPosition;
+    public float restrictionX;
+    public float restrictionY;
+    public float restrictionZ;
+    public float detectionSphereRadius;
+
+    public int spawnLimit;
+    int spawnAmount;
+    bool firstSpawn;
+    public float spawnTime;
+    float time_remaining;
+    float runningTime;
+
+    Vector3 cameraPosition;
+    void Start()
+    {
+        spawnAmount = 0;
+        firstSpawn = false;
+
+        time_remaining = spawnTime;
+        runningTime = 0;
+
+        cameraPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       cameraPosition = transform.position;
+       runningTime = runningTime + Time.deltaTime;
+
+        CheckVisibleGhost();
+
+       if(time_remaining > 0)
+       {
+            time_remaining = time_remaining - Time.deltaTime;
+       }
+       else
+       {
+            if(spawnAmount > spawnLimit)
+            {
+                spawnAmount = 0;
+                firstSpawn = true;
+            }
+            if(!firstSpawn)
+            {
+                spawnPosition = new Vector3(Random.Range(cameraPosition.x - restrictionX, cameraPosition.x + restrictionX), Random.Range(cameraPosition.y, cameraPosition.y + restrictionY), Random.Range(cameraPosition.z - restrictionZ, cameraPosition.z + restrictionZ));
+                spawnedObjects.Add(Instantiate(spawnablePrefab, spawnPosition, Quaternion.identity));
+                if (Physics.CheckSphere(cameraPosition, detectionSphereRadius))
+                {
+                    Destroy(spawnedObjects[spawnAmount]);
+                    spawnedObjects.RemoveAt(spawnAmount);
+                    goto continueProcess;
+                }
+            }
+            else
+            {
+                Destroy(spawnedObjects[spawnAmount]);
+                spawnedObjects.RemoveAt(spawnAmount);
+
+                spawnPosition = new Vector3(Random.Range(cameraPosition.x - restrictionX, cameraPosition.x + restrictionX), Random.Range(cameraPosition.y, cameraPosition.y + restrictionY), Random.Range(cameraPosition.z - restrictionZ, cameraPosition.z + restrictionZ));
+                spawnedObjects.Insert(spawnAmount, Instantiate(spawnablePrefab, spawnPosition, Quaternion.identity));
+                if (Physics.CheckSphere(cameraPosition, detectionSphereRadius))
+                {
+                    goto continueProcess;
+                }
+            }
+
+            spawnAmount++;
+            time_remaining = spawnTime;
+       }
+
+    continueProcess:;
+    }
+
+    private void CheckVisibleGhost()
+    {
+        
+    }
+}
