5ba55a86b458011ddcd24bbb6e718b4cbe3e121d
BoyanKardzhiev <b.r.kardzhiev@student.tue.nl>
2023-11-24 10:51:45 +0100

    Ghost Changes Velocity

--- Stage 3/Assets/Scripts/GhostSpawnManager.cs
+++ Stage 3/Assets/Scripts/GhostSpawnManager.cs
@@ -33,13 +33,15 @@ public class GhostSpawnManager : MonoBehaviour
         time_remaining = spawnTime;
         runningTime = 0;
 
-        cameraPosition = Camera.main.transform.position;
+        cameraPosition = transform.position;
+        //cameraPosition = Camera.main.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-       cameraPosition = Camera.main.transform.position;
+       //cameraPosition = Camera.main.transform.position;
+       cameraPosition = transform.position;
        runningTime = runningTime + Time.deltaTime;
 
         CheckVisibleGhost();
